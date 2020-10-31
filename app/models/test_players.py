3d44fddd68f7b50ef6d0ad3f53c6b9c3d5da4b2a
Ian F <ian.fiddeman@gmail.com>
2020-10-31 11:11:55 +0000

    2nd commit

--- app/models/test_players.py
+++ app/models/test_players.py
@@ -1,2 +1,14 @@
+import unittest
+from models.player_class import Player
 
+class TestPlayer(unittest.TestCase):
+
+    def setUp(self):
+        self.player = Player("John", "Rock")
+
+    def test_player_has_name(self):
+        self.assertEqual("John", self.player.player_name)
+    
+    def test_player_has_choice(self):
+        self.assertEqual("Rock", self.player.choice)
 
---
+++ app/run_tests.py
@@ -0,0 +1,6 @@
+import unittest
+
+from models.test_players import TestPlayer
+
+if __name__ == '__main__':
+    unittest.main()
