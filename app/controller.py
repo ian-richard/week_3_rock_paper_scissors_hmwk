431b49860f48f6a2cd9a78cb0f25e4a504c4ba2a
Ian F <ian.fiddeman@gmail.com>
2020-11-01 18:02:19 +0000

    nearly MVP

--- app/controller.py
+++ app/controller.py
@@ -8,7 +8,7 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/add-match', methods=['POST'])
+@app.route('/results', methods=['POST'])
 def add_player():
     P1_name = request.form['P1_name']
     P2_name = request.form['P2_name']
@@ -16,5 +16,8 @@ def add_player():
     P2_choice = request.form['P2_choice']
     player_1 = Player(player_name=P1_name, choice=P1_choice)
     player_2 = Player(player_name=P2_name, choice=P2_choice)
-    winner_in_list(player_1, player_2)
-    return redirect('/')
+    my_game = Game(player_1, player_2)
+    
+    result = my_game.winner_in_list(player_1, player_2)
+    
+    return render_template('results.html', result=result)
